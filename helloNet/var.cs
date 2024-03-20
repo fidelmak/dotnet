6dcf0ac55211d4ee63a689b6c097a5a23340b1be
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2024-03-20 19:19:14 +0100

    update

--- helloNet/var.cs
+++ helloNet/var.cs
@@ -7,3 +7,22 @@ Console.WriteLine(centuries + "centuries are " + years + " years, or " + days + 
 
 ulong maxIntValue = UInt64.MaxValue;
 Console.WriteLine(maxIntValue);
+
+// practice from freecodecamp
+
+Console.WriteLine("Hello\nWorld");
+Console.WriteLine("Hello\tWorld");
+
+Console.WriteLine("Hello \"World\"!");
+Console.WriteLine("c:\\source\repos");
+
+
+Console.WriteLine("Generating invpoces for customer \"Catose Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory: \t");
+
+// verbatim string literal 
+
+Console.WriteLine(@" c:\source\repos(this is where your code goes)");
+
